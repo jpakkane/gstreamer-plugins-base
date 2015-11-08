eebe2b26308f9464a109175b5d3065b45f052022
Jussi Pakkanen <jpakkane@gmail.com>
2015-11-08 23:26:24 +0200

    More marshalling and enuming.

---
+++ gst-libs/gst/rtsp/rtsp_marshal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys, subprocess
+
+assert(len(sys.argv) == 3)
+
+_, listname, outname = sys.argv
+
+if outname.endswith('.h'):
+    arg = '--header'
+else:
+    arg = '--body'
+
+output = subprocess.check_output(['glib-genmarshal', '--prefix=__gst_rtsp_marshal', arg, listname])
+open(outname, 'wb').write(output)
